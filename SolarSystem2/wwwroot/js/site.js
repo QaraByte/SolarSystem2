1cab34b311412cdc3d998e4ddf291ede7a4c7aa5
QaraByte <pchelpkz16@gmail.com>
2022-11-13 21:51:00 +0600

    Add project files.

---
+++ SolarSystem2/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+    let solarTop = $('.solar').offset().top;
+    let solarLeft = $('.solar').offset().left;
+
+    let earth = $('<div></div>').addClass('earth');
+    $('.pb-3').append(earth);
+    $(earth).offset({ top: solarTop - 70 });
+    $(earth).offset({ left: solarLeft + 70 });
+});
